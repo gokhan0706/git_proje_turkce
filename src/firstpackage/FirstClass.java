2ee3974723a5552c3f83a7821a74605e1b478287
Gokhan CILI <gokhancili8607@gmail.com>
2020-06-20 20:14:15 +0200

    first commit from local repo

---
+++ src/firstpackage/FirstClass.java
@@ -0,0 +1,17 @@
+package firstpackage;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+
+System.out.println("first commit from local repository");
+
+	}
+
+}
+//hocam kisaca ozetleyelim:
+//1-GitHub hesabi actik
+//2-Sag ustteki arti tusunun oradaki ok a tiklayip new repository dedik
+//3- eclipseye git yeni proje ac. adi: git_proje_turkce
+//4-icinde package olustur adi: firstpackage
+//5-class olustur adi: FirstClass
