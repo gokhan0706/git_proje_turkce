020b019284a224cf457b30dcc3b284be10324c08
gokhan0706 <57571309+gokhan0706@users.noreply.github.com>
2020-06-20 21:45:36 +0200

    Update FirstClass.java
    conflict kazasi

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,12 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 
 System.out.println("first commit from local repository");
-System.out.println("second commitfrom local repository");
-<<<<<<< HEAD
 System.out.println("ucuncu commitimiz local repositoryden");
-=======
-System.out.println("conflict yapmak istiyoruz");
->>>>>>> refs/remotes/origin/master
+System.out.println("we are againts to conflicts");
 System.out.println("third commit from local repository");
 		
 
